c1c068e0efe31218a6344ebabaa12c835d40d59e
Félix <legrelle.f@gmail.com>
2020-10-16 13:20:19 +0200

    Ajout de la navbar et de la favicon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,14 @@ export default class Header extends React.Component {
     render() {
 
         return (
-            <nav>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={process.env.PUBLIC_URL + "icon.png"} alt="" width="30" height="30"
+                             className="d-inline-block align-top mr-2" />
+                            Menu CROUS
+                    </a>
+                </div>
             </nav>
         );
     }
--- src/components/Index/index.js
+++ src/components/Index/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import axios from 'axios'
 
 export default class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            menu:[],
+            cafet:15
+        }
+    }
+    componentWillMount() {
+        axios
+            .get("https://gddl.crous-versailles.fr/vae/api/menus/date_and_lieu/1603362756000/"+this.state.cafet)
+            .then((reponse) =>{
+                console.log(reponse)
+            })
+    }
+
     render(){
         return (
             <div>

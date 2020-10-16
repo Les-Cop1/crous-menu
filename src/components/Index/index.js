3b6c73fa43a2ad31ee7eeda93540973d0290be49
Félix <legrelle.f@gmail.com>
2020-10-16 15:43:43 +0200

    ajout variable dans le state

--- src/components/Index/index.js
+++ src/components/Index/index.js
@@ -5,16 +5,23 @@ export default class Index extends React.Component {
     constructor(props) {
         super(props);
         this.state={
+            timestamp: Math.floor(Date.now() / 1000),
             menu:[],
-            cafet:15
+            cafet:15,
+            loading:false
         }
     }
     componentWillMount() {
         axios
-            .get("https://gddl.crous-versailles.fr/vae/api/menus/date_and_lieu/1603362756000/"+this.state.cafet)
+            .get("https://gddl.crous-versailles.fr/vae/api/menus/date_and_lieu/"+this.state.timestamp+"000/"+this.state.cafet)
             .then((reponse) =>{
                 console.log(reponse)
-            })
+                this.setState({menu:reponse.data[0].plat.libelle.split(', ')})
+                console.log(this.state)
+            }).catch((status)=>{
+                console.log("erreur de requete GET")
+        })
+
     }
 
     render(){
